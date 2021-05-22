f98d2808249060ff49c0c47c9ccef8f71a201e72
crealyx <sanberk5416@hotmail.com>
2021-05-22 20:28:44 +0300

    Create elements with javascript

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import "./assets/styles/main.scss";
 
 import { createTodo } from "./assets/js/newTodo";
+import {} from "./assets/js/dom";
 
 // HMR
 if (module.hot) {
   module.hot.accept();
 }
 //
-console.log(createTodo("Work", "workday", "today"));
-console.log(createTodo("Dance", "danceday", "today"));
